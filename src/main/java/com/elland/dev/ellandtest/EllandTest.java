a21548ffba74d08fcf7dde0e44f29852ab5283e5
Marshall_Oh <sh.oh@urbandigital.com>
2023-04-18 17:34:44 +0900

    generate dpm-start cmd

--- src/main/java/com/elland/dev/ellandtest/EllandTest.java
+++ src/main/java/com/elland/dev/ellandtest/EllandTest.java
@@ -1,5 +1,6 @@
 package com.elland.dev.ellandtest;
 
+import com.elland.dev.ellandtest.cmds.DpmStartCommand;
 import com.elland.dev.ellandtest.cmds.TestMobCommand;
 import com.elland.dev.ellandtest.events.DamageEvents;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,10 +8,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
 
 public final class EllandTest extends JavaPlugin {
-    public static HashMap<String, Integer> dpmMap = new HashMap<String, Integer>();
+    public static HashMap<String, Integer> dpmMap = new HashMap<>();
 
     @Override
     public void onEnable() {
+        getCommand("dpm-start").setExecutor(new DpmStartCommand());
         getCommand("test-mob").setExecutor(new TestMobCommand());
         getServer().getPluginManager().registerEvents(new DamageEvents(), this);
     }
