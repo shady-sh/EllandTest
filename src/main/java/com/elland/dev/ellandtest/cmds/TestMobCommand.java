3a157da1680372a0652df763fd50353e40d0cf60
Marshall_Oh <sh.oh@urbandigital.com>
2023-04-29 17:00:02 +0900

    테스트몹 체력 설정 기능 추가

--- src/main/java/com/elland/dev/ellandtest/cmds/TestMobCommand.java
+++ src/main/java/com/elland/dev/ellandtest/cmds/TestMobCommand.java
@@ -19,25 +19,26 @@ public class TestMobCommand implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "플레이어만 이 명령어를 사용할 수 있습니다.");
             return true;
         }
+        Player player = (Player) sender;
 
-        if (args.length == 0) {
-            sender.sendMessage(ChatColor.GOLD + "/테스트몹 <숫자> - 숫자 마리 만큼 테스트 몹을 소환합니다.");
+        if (args.length < 2) {
+            sender.sendMessage(ChatColor.GOLD + "/테스트몹 <체력> <숫자> - 체력과 숫자를 입력해주세요.");
             return true;
         }
-
+        int health;
         int number;
         try {
-            number = Integer.parseInt(args[0]);
+            health = Integer.parseInt(args[0]);
+            number = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
             sender.sendMessage(ChatColor.RED + "숫자를 입력해주세요.");
             return true;
         }
 
-        Player player = (Player) sender;
         for (int i = 0; i < number; i++) {
             Zombie zombie = (Zombie) player.getWorld().spawnEntity(player.getLocation(), EntityType.ZOMBIE);
-            zombie.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(50000.0);
-            zombie.setHealth(50000.0);
+            zombie.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue((double) health);
+            zombie.setHealth((double) health);
             zombie.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 10000000, 1));
             zombie.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 10000000, 1));
             zombie.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 10000000, 0));
@@ -45,8 +46,8 @@ public class TestMobCommand implements CommandExecutor {
             zombie.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 1000000, 0));
             zombie.setCustomName(ChatColor.GREEN + "테스트 몹");
         }
-
-        sender.sendMessage(ChatColor.YELLOW + String.valueOf(number) + "마리의 테스트 몹이 소환되었습니다.");
+        sender.sendMessage(ChatColor.YELLOW + String.valueOf(number) + "마리의 체력 " + health + "의 테스트 몹이 소환되었습니다.");
         return true;
     }
 }
+
